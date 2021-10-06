f677377545cb885aaf375172bda86be7221d201d
Markus Donko-Huber <markus@nysos.net>
2021-10-06 10:20:08 +0200

    Support for nameserver kubernetes pods

--- cryptodns/bin/run.sh
+++ cryptodns/bin/run.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-dns_ips=$(getent hosts nameserver | awk 'BEGIN{ORS=":5300 ";}{print $1}')
+if [[ -z "${POD_NAME}" ]]; then
+  service_host=${POD_NAME}_NAMESERVER_SERVICE_HOST
+  service_port=${POD_NAME}_NAMESERVER_SERVICE_PORT
+  dns_ips="${service_host}:${service_port}"
+else
+  dns_ips=$(getent hosts nameserver | awk 'BEGIN{ORS=":5300 ";}{print $1}')
+fi
+
 dnsdist --supervised $dns_ips
