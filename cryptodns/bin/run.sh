34737f074a4856ed0f861537bbfa9f192b5fa754
Markus Donko-Huber <markus@nysos.net>
2021-10-06 13:59:49 +0200

    Fixed getting nameserver pod service

--- cryptodns/bin/run.sh
+++ cryptodns/bin/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ -z "${POD_NAME}" ]]; then
-  service_host=${POD_NAME}_NAMESERVER_SERVICE_HOST
-  service_port=${POD_NAME}_NAMESERVER_SERVICE_PORT
-  dns_ips="${service_host}:${service_port}"
+if [[ -v POD_NAME ]]; then
+  service_host="${POD_NAME}_NAMESERVER_SERVICE_HOST"
+  service_port="${POD_NAME}_NAMESERVER_SERVICE_PORT"
+  dns_ips="${!service_host}:${!service_port}"
 else
   dns_ips=$(getent hosts nameserver | awk 'BEGIN{ORS=":5300 ";}{print $1}')
 fi
